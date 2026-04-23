f2eef49981e79e4f2c5d3d60978a4d8c2ab00bab
sig5 <singhalsakar@gmail.com>
2026-04-24 01:29:01 +0530

    add kv db layer

---
+++ db/db.go
@@ -0,0 +1,98 @@
+package db
+
+import (
+	"fmt"
+	"lorem-lsm/memtable"
+	"lorem-lsm/sstable"
+	"lorem-lsm/wal"
+	"time"
+)
+
+type LoremDB struct {
+	wal         *wal.Wal
+	memTable    *memtable.MemTable
+	ssTables    []*sstable.SSTable
+	ssTablePath string
+}
+
+func NewLoremDB() *LoremDB {
+
+	wal, _ := wal.NewWal("wal.log")
+	memTable := memtable.NewMemTable()
+	ssTables := []*sstable.SSTable{}
+	ssTablePath := "sstable"
+
+	return &LoremDB{
+		wal:         wal,
+		memTable:    memTable,
+		ssTables:    ssTables,
+		ssTablePath: ssTablePath,
+	}
+}
+
+func (db *LoremDB) Put(key string, value string) error {
+	// write wal first to maximize data recovery chances
+	db.wal.Append(wal.WalRow{
+		Key:       key,
+		Value:     value,
+		IsDeleted: false,
+	})
+
+	isMemTableFull := db.memTable.Put(key, value)
+
+	if isMemTableFull {
+		path := fmt.Sprintf("%s/%d", db.ssTablePath, time.Now().UnixNano())
+		table, err := sstable.CreateSSTable(path)
+
+		if err != nil {
+			return err
+		}
+
+		table.FlushMemTable(db.memTable)
+		db.ssTables = append(db.ssTables, table)
+		db.memTable = memtable.NewMemTable()
+	}
+	return nil
+}
+
+func (db *LoremDB) Delete(key string) error {
+	// write wal first to maximize data recovery chances
+	db.wal.Append(wal.WalRow{
+		Key:       key,
+		Value:     "",
+		IsDeleted: true,
+	})
+
+	isMemTableFull := db.memTable.Delete(key)
+
+	if isMemTableFull {
+		path := fmt.Sprintf("%s/%d", db.ssTablePath, time.Now().UnixNano())
+		table, err := sstable.CreateSSTable(path)
+
+		if err != nil {
+			return err
+		}
+
+		table.FlushMemTable(db.memTable)
+		db.ssTables = append(db.ssTables, table)
+		db.memTable = memtable.NewMemTable()
+	}
+	return nil
+}
+
+func (db *LoremDB) Get(key string) (string, bool) {
+	// check in memtable
+	val, ok := db.memTable.Get(key)
+	if ok {
+		return val, true
+	}
+	// fallback to sstable
+	for i := len(db.ssTables) - 1; i >= 0; i-- {
+		value := db.ssTables[i].Get(key)
+
+		if value != "" {
+			return value, true
+		}
+	}
+	return "", false
+}
