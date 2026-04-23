2d002975be34af4294229e6c9afb2f4988b7097a
sig5 <singhalsakar@gmail.com>
2026-04-24 03:08:26 +0530

    bugfixes

--- main.go
+++ main.go
@@ -2,17 +2,55 @@ package main
 
 import (
 	"fmt"
-	"lorem-lsm/bloom"
+	"lorem-lsm/db"
+	"time"
 )
 
 func main() {
-	testFilter := bloom.NewBloomFilter(1000, 5)
-	testFilter.Add("test")
-	fmt.Println(testFilter.Contains("test"))
-	fmt.Println(testFilter)
-	// should be false, but might return true (false positive)
-	fmt.Println(testFilter.Contains("gopher"))
-	fmt.Println(testFilter.Contains("rust"))
-	fmt.Println(testFilter.Contains("lsm"))
+	// store := db.NewLoremDB(true)
+
+	// store.Put("name", "sakar")
+	// store.Put("lang", "go")
+	// store.Put("project", "lsm")
+
+	// val, ok := store.Get("name")
+	// fmt.Println(val, ok) // sakar true
+
+	// store.Delete("lang")
+	// val, ok = store.Get("lang")
+	// fmt.Println(val, ok) // "" false
+
+	// val, ok = store.Get("missing")
+	// fmt.Println(val, ok) // "" false
+
+	run_benchmark(true)
+	run_benchmark(false)
+}
+
+func run_benchmark(useBloom bool) {
+	fmt.Println("--- With bloom filter --- %s", useBloom)
+
+	store := db.NewLoremDB(useBloom)
+
+	// benchmark writes
+	start := time.Now()
+	for i := 0; i < 500001; i++ {
+		store.Put(fmt.Sprintf("key-%d", i), fmt.Sprintf("value-%d", i))
+	}
+	fmt.Printf("10k writes: %v\n", time.Since(start))
+
+	// benchmark reads (keys that exist)
+	start = time.Now()
+	for i := 0; i < 50000; i++ {
+		store.Get(fmt.Sprintf("key-%d", i))
+	}
+	fmt.Printf("10k reads (hits): %v\n", time.Since(start))
+
+	// benchmark reads (keys that don't exist)
+	start = time.Now()
+	for i := 0; i < 50000; i++ {
+		store.Get(fmt.Sprintf("missing-%d", i))
+	}
+	fmt.Printf("10k reads (misses): %v\n", time.Since(start))
 
 }
